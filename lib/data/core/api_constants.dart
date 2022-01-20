a9d5439bf7752099b51914673d8db5972ce857c8
GIGCO DEV 1 <gigcodev1@GIGCOs-Mac-mini-2.local>
2022-01-20 17:57:48 +0700

    add api key for tmdb api

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,11 @@
+
+
+
+class ApiConstants {
+
+  ApiConstants.();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "de976ae0e09878061da85cd2eb3b4aaf";
+
+}
